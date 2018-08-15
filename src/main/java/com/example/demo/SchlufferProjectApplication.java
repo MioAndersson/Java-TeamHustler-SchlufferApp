5246a1c25fa374e890c4d828519cc0ed21fe6fa1
vonzombie <vonzombie@yahoo.com>
2018-08-15 10:21:40 -0700

    Foundation for BackEnd

---
+++ src/main/java/com/example/demo/SchlufferProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchlufferProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchlufferProjectApplication.class, args);
+	}
+}
